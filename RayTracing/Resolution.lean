cea1ecc6c5413e6e856de7c45f5f6c18d1ed7c5a
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-07-15 14:44:37 -0600

    use Rat instead of Float for aspect ratio

--- RayTracing/Resolution.lean
+++ RayTracing/Resolution.lean
@@ -3,15 +3,24 @@ namespace resolution
 public structure Resolution where
   rowCount : Nat
   columnCount : Nat
+  deriving Repr
 
 namespace Resolution
+
+open Resolution
 
 public abbrev Float.toNat (f : Float) : Nat :=
   f.toUInt64.toNat
 
-public def fromColumnCountIdealAspectRatio (columnCount : Nat) (idealAspectRatio : Float) : Resolution :=
-  let rowCount := Float.toNat (columnCount.toFloat / idealAspectRatio)
-  ⟨if rowCount < 1 then 1 else rowCount, columnCount⟩
+public abbrev Rat.absFloat (r : Rat) : Float :=
+  r.abs.num.toNat.toFloat / r.den.toFloat
+
+public def fromColumnCountIdealAspectRatio
+  (columnCount : Nat) (idealAspectRatio : Rat)
+  : Resolution :=
+  let rowCount := Float.toNat (columnCount.toFloat / idealAspectRatio.absFloat)
+  let rowCount := if rowCount < 1 then 1 else rowCount
+  ⟨rowCount, columnCount⟩
 
 public def aspectRatio [Coe Nat β] [Div β] (self : Resolution) : β :=
   (self.columnCount : β) / (self.rowCount : β)
