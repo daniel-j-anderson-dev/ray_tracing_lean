dc4158dbccc3b43e0027ba561b2b01632fcf3c45
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-07-15 14:46:07 -0600

    add example using new types

--- RayTracing/Example.lean
+++ RayTracing/Example.lean
@@ -2,17 +2,22 @@ import RayTracing.Resolution
 import RayTracing.Netpbm
 import RayTracing.Basic
 import RayTracing.Ray
+import RayTracing.Camera
+import RayTracing.Viewport
 
 open rgb
 open resolution
 open uint8_max
 
+abbrev Scalar := Float
+abbrev Vector3 := vector3.Vector3 Scalar
+abbrev Ray := ray.Ray Scalar
+abbrev Camera := camera.Camera Scalar
+abbrev Viewport := viewport.Viewport Scalar
+abbrev Viewport.mk' := viewport.Viewport.mk' (α := Scalar) (NatToα := ⟨Nat.toFloat⟩)
+
 def outputPathRoot := "./output/"
 def outputPathExtension := ".ppm"
-
-def Scalar := Float
-def Vector3 := vector3.Vector3 Scalar
-def Ray := ray.Ray Scalar
 
 namespace red_green_gradient
 
@@ -45,3 +50,21 @@ def outputPath := s!"{outputPathRoot}red_green_gradient{outputPathExtension}"
 #eval IO.FS.writeBinFile outputPath imageData
 
 end red_green_gradient
+
+namespace ray_cast
+
+
+-- image
+
+def idealAspectRatio := mkRat 16 9
+def imageWidth := 400
+def resolution := Resolution.fromColumnCountIdealAspectRatio imageWidth idealAspectRatio
+
+def camera : Camera := {
+  center := ⟨0,0,0⟩
+  orientation := ⟨0, 0, 0, 0⟩
+}
+
+def viewport := Viewport.mk' (focalLength := 1.0) (height := 2.0) camera resolution
+
+end ray_cast
