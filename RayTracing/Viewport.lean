3788079b487ff092c5c7a57bf52fb712a29849aa
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-07-15 12:31:04 -0600

    added viewport type

---
+++ RayTracing/Viewport.lean
@@ -0,0 +1,46 @@
+import RayTracing.Vector3
+import RayTracing.Camera
+import RayTracing.Resolution
+
+namespace viewport
+
+open resolution
+open vector3
+open camera
+
+public structure Viewport α where
+  --- The center of pixel at row 0, column 0
+  topLeft : Vector3 α
+
+  --- The "horizontal" change between pixels
+  ΔX : Vector3 α
+
+  --- The "vertical" change between pixels
+  ΔY : Vector3 α
+
+namespace Viewport
+
+public def mk'
+  [Add α] [Sub α] [Neg α] [Mul α] [Div α] [Zero α] [One α] [Coe Nat α]
+  (focalLength : α) (height : α) (camera : Camera α) (resolution : Resolution)
+  : Viewport α :=
+  let two : α := 1 + 1
+
+  let width := height * resolution.aspectRatio
+
+  let x := camera.right * width
+  let y := camera.up * -height
+  let z := camera.forward * focalLength
+
+  let center := camera.center + z
+  let topLeft := center -((x + y) / two)
+
+  let pixelΔX := x / (resolution.columnCount : α)
+  let pixelΔY := y / (resolution.rowCount : α)
+  let pixelTopLeft := topLeft + pixelΔX + pixelΔY
+
+  ⟨pixelTopLeft, pixelΔX, pixelΔY⟩
+
+end Viewport
+
+end viewport
