32c45dcbb055b4c6ea2a36fbec0332087dd49f13
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-07-19 14:52:06 -0600

    fixed Hittable signature

--- RayTracing/Hit.lean
+++ RayTracing/Hit.lean
@@ -7,29 +7,20 @@ open vector3
 open ray
 
 namespace hit_record
+
 public structure HitRecord α where
   position : Vector3 α
   normal : Vector3 α
   t : α
 
-namespace HitRecord
-
-public instance [Inhabited α] : Inhabited (HitRecord α) where
-  default :=
-  let a: α := Inhabited.default
-  {
-    position := ⟨a, a, a⟩
-    normal := ⟨a, a, a⟩
-    t := a
-  }
-
-end HitRecord
 end hit_record
+
 namespace hittable
 
 open hit_record in
-public class Hittable α extends (Inhabited α) where
-  hit (ray : Ray α) (rayTMin rayTMax : α) : Option (HitRecord α)
+public class Hittable (Self Scalar : Type u) where
+  hit (self : Self) (ray : Ray Scalar) (rayTMin rayTMax : Scalar) : Option (HitRecord Scalar)
 
 end hittable
+
 end hit
