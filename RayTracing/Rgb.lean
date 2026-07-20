685369d4670637db26aa3712a184c4084fc2080e
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-07-20 12:14:12 -0600

    made Rgb more transparent

--- RayTracing/Rgb.lean
+++ RayTracing/Rgb.lean
@@ -5,7 +5,7 @@ namespace rgb
 
 open vector3
 
-public abbrev Rgb := Vector3
+public abbrev Rgb := @Vector3
 public abbrev rgb (red green blue : α) : Rgb α := ⟨red, green, blue⟩
 public abbrev vector3.Vector3.red := @Vector3.x
 public abbrev vector3.Vector3.green := @Vector3.y
